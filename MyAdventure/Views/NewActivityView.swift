5a7d17caa401c6e2d26165a745c471b4980a99ab
dustom <dusek.tomas@icloud.com>
2025-01-22 20:51:36 +0100

    initial setup

---
+++ MyAdventure/Views/NewActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  NewActivityView.swift
+//  MyAdventure
+//
+//  Created by Tomáš Dušek on 22.01.2025.
+//
+
+import SwiftUI
+
+struct NewActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewActivityView()
+}
